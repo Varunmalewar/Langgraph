7a24332c1040d0c82631f6922633390458d43b38
Varun Malewar <vmmalewar@gmail.com>
2026-09-08 10:50:57 +0530

    Add backend and frontend for chatbot functionality with Google Generative AI integration

---
+++ chatbot_with_short_term_memory/langgraph_backend.py
@@ -0,0 +1,47 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+import os
+from pydantic import SecretStr,BaseModel , Field 
+from typing import TypedDict,NotRequired,Annotated, Literal
+from langgraph.graph import StateGraph, START, END
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage, BaseMessage
+import operator
+from langgraph.graph.message import add_messages
+from langgraph.checkpoint.memory import MemorySaver # ram wali memory
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(
+    model = "gemini-3.5-flash-lite",
+    api_key = SecretStr(os.environ["GOOGLE_API_KEY"]),
+)
+
+
+class ChatState(TypedDict):
+    messages : Annotated[list[BaseMessage], add_messages]
+
+
+def chat_node(state : ChatState) :
+    # take user query from state 
+    messages = state['messages']
+
+
+    # send tp the llm 
+    response = llm.invoke(messages)
+
+    # response store state 
+    return{
+        'messages':[response]
+    }
+
+
+checkpointer = MemorySaver()
+graph = StateGraph(ChatState)
+
+graph.add_node('chat_node',chat_node)
+
+graph.add_edge(START,'chat_node')
+graph.add_edge('chat_node',END)
+
+workflow = graph.compile(checkpointer=checkpointer)
---
+++ chatbot_with_short_term_memory/streamlit_frontend.py
@@ -0,0 +1,36 @@
+from langchain_core.messages import HumanMessage
+import streamlit as st
+from langgraph_backend import workflow
+
+
+config1 = {
+    "configurable":{
+        "thread_id": "thread-1"
+    }
+}
+# st.session_state ->dict
+if "message_history" not in st.session_state:
+    st.session_state.message_history = []
+
+message_history = st.session_state.message_history
+
+for message in message_history :
+    with st.chat_message(message["role"]):
+        st.text(message["content"])
+
+user_input = st.chat_input("Type your message here")
+
+if user_input:
+
+    #first add the message to message
+    st.session_state.message_history.append({"role":"user","content":user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
+
+    response = workflow.invoke({"messages" : [HumanMessage(content=user_input)]},config=config1)
+    response['messages'][-1].text
+    st.session_state.message_history.append({"role":"assistant","content":response['messages'][-1].text})
+    with st.chat_message('assistant'):
+        st.text(response['messages'][-1].text)
+
+
